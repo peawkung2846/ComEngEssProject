89ab6f08c1b85bae1d4ce00ba08fcffff468fb46
Boriphat Aranyakanon <boriphataranyakanon@gmail.com>
2024-04-14 17:21:50 +0700

    select image

--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -1,4 +1,4 @@
-var img = document.getElementById("popcat1");
+var img = document.getElementById("selectedImage");
 var count = document.getElementById("score");
 var malaysiaScore = document.getElementById('my_score');
 var score = 0;
@@ -7,27 +7,50 @@ var audio = new Audio('pop.mp3');
 var hk_score = 1000, tw_score = 430, th_score = 200, jp_score = 150, fi_score = 250, se_score = 100, pl_score = 500, dm_score = 280, id_score=590, hu_score=319, sr_score = 300; 
 
 // mouseclick event
-img.addEventListener("mousedown", function(){
+
+img.addEventListener("mousedown", function(event){
     increaseScore();
-    img.src = 'popcat2.png';
-    audio.play();
+    if (img.src.includes('popcat1.png')) {
+        img.src = 'popcat2.png';
+        audio.currentTime = 0;
+        audio.play();
+    }
+    else if(img.src.includes('popcat2.png')){
+        img.src = 'popcat1.png';
+        audio.currentTime = 0;
+        audio.play();
+    }
 });
-    
-img.addEventListener("mouseup", function(){
-    img.src = 'popcat1.png';
-    audio.play();
+
+img.addEventListener("mouseup", function(event){
+    if (img.src.includes('popcat2.png')) {
+        img.src = 'popcat1.png';
+    }
+    else if(img.src.includes('popcat1.png')){
+        img.src = 'popcat2.png';
+    }
 });
 
 // touch event
-img.addEventListener("touchstart", function(){
+img.addEventListener("touchstart", function(event){
     increaseScore();
-    img.src = 'popcat2.png';
-    audio.play();
+    if (img.src.includes('popcat1.png')) {
+        img.src = 'popcat2.png';
+        audio.play();
+    }
+    else if(img.src.includes('popcat2.png')){
+        img.src = 'popcat1.png';
+        audio.play();
+    }
 });
 
-img.addEventListener("touchmove", function(){
-    img.src = 'popcat1.png';
-     audio.play();
+img.addEventListener("touchmove", function(event){
+    if (img.src.includes('popcat2.png')) {
+        img.src = 'popcat1.png';
+    }
+    if (img.src.includes('popcat1.png')) {
+        img.src = 'popcat2.png';
+    }
 });
 
 
@@ -115,4 +138,15 @@ function increaseScore(){
     count.innerHTML = score;
     malaysiaScore.innerHTML = MyScore;
     document.getElementById("table").rows[5].cells.item(3).innerHTML = MyScore;
+}
+
+function toggleOptions() {
+    var options = document.getElementById('options');
+    options.style.display = options.style.display === 'block' ? 'none' : 'block';
+}
+
+function changeImage(image) {
+    img.src = image;
+    document.getElementById('selectedImage').src = image;
+    toggleOptions();
 }
