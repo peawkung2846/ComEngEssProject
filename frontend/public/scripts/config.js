3d2ea04a39be18b9bf265a8400ab80641988c5f8
Boriphat Aranyakanon <boriphataranyakanon@gmail.com>
2024-04-14 15:10:14 +0700

    frontend

---
+++ frontend/public/scripts/config.js
@@ -0,0 +1,10 @@
+// TODO0: change the BACKEND_URL to your EC2 public IPv4
+export const BACKEND_URL = "http://34.224.5.84:3222";
+
+// TODO0: change element in MEMBERS to your group members.
+// WARNING: the _id parameters is required.
+export const MEMBERS = [
+  { name: "อนวัช บำรุงศรี", _id: 1 },
+  { name: "ศุภวิชญ์ เจนสวัสดิ์พงศ์", _id: 2 },
+  { name: "วฤทธิ์ ศรีสุพรรณทอง", _id: 3 },
+];
