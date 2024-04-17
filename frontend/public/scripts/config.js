2bc09fe222419bd8aec385bec4e0eaf2e7888a1d
Boriphat Aranyakanon <boriphataranyakanon@gmail.com>
2024-04-17 22:22:54 +0700

    Change backend ip

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,5 +1,5 @@
 // TODO0: change the BACKEND_URL to your EC2 public IPv4
-export const BACKEND_URL = "http://localhost:3222";
+export const BACKEND_URL = "44.223.218.147";
 
 // TODO0: change element in MEMBERS to your group members.
 // WARNING: the _id parameters is required.
